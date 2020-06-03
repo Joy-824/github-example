7a12e7e4fc2a373021c79d7e391197e7a55bb392
Joy-824 <66305421+Joy-824@users.noreply.github.com>
2020-06-03 15:47:12 +0800

    Capstone_Project

---
+++ Capstone Project 1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is for Coursera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
